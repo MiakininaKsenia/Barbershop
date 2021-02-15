78b464558691567675d994d0354e011ac52ca5d9
Ksenia Myakinina <ksunya1619@gmail.com>
2021-02-15 19:36:20 +0200

    test

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,16 @@
-$(document).ready(function() {
-	// $('body').hide()
-})
+$(document).ready(function () {
+	const swiper = new Swiper('.swiper-container', {
+		loop: true,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		autoplay: {
+			delay: 5000,
+		}
+	});
+
+	$('.header-container').on('click', '.menu-button', function () {
+		$('.header-container').toggleClass('menu-opened')
+	})
+});
