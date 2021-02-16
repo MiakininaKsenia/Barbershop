a0b38e215701351602e73f3dbdfb24ad78a78c38
Ksenia Myakinina <ksunya1619@gmail.com>
2021-02-16 21:39:23 +0200

    test

--- js/app.js
+++ js/app.js
@@ -13,4 +13,20 @@ $(document).ready(function () {
 	$('.header-container').on('click', '.menu-button', function () {
 		$('.header-container').toggleClass('menu-opened')
 	})
+
+	//login modal
+	$('.js-login').on('click', function (e) {
+		e.preventDefault();
+		$('.modal--login').fadeIn(400);
+	});
+	$('.modal__close').on('click', function (e) {
+		e.preventDefault();
+		$('.modal--login').fadeOut(400);
+	});
+	$(document).on('mouseup', function (event) {
+		const LoginModal = $('.modal__window');
+		if (!LoginModal.is(event.target) && LoginModal.has(event.target).length === 0) {
+			$('.modal').fadeOut(400);
+		}	
+	})
 });
